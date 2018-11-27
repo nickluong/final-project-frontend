58a1bd6f761ca9ce4656e2c26050ca01d848e719
Nick Luong <nckluong@gmail.com>
2018-11-27 17:17:02 -0600

    set up homepage but no login or signup yet

---
+++ material-kit-react-v1.3.0/src/views/LandingPage/Sections/ProductSection.jsx
@@ -0,0 +1,69 @@
+import React from "react";
+// @material-ui/core components
+import withStyles from "@material-ui/core/styles/withStyles";
+
+// @material-ui/icons
+import Chat from "@material-ui/icons/Chat";
+import VerifiedUser from "@material-ui/icons/VerifiedUser";
+import Waves from "@material-ui/icons/Waves";
+// core components
+import GridContainer from "components/Grid/GridContainer.jsx";
+import GridItem from "components/Grid/GridItem.jsx";
+import InfoArea from "components/InfoArea/InfoArea.jsx";
+
+import productStyle from "assets/jss/material-kit-react/views/landingPageSections/productStyle.jsx";
+
+class ProductSection extends React.Component {
+  render() {
+    const { classes } = this.props;
+    return (
+      <div className={classes.section}>
+        <GridContainer justify="center">
+          <GridItem xs={12} sm={12} md={8}>
+            <h2 className={classes.title}>Let's talk about the project</h2>
+            <h5 className={classes.description}>
+              This is the paragraph where you can write more details about your
+              product. Keep you user engaged by providing meaningful
+              information. Remember that by this time, the user is curious,
+              otherwise he wouldn't scroll to get here. Add a button if you want
+              the user to see more.
+            </h5>
+          </GridItem>
+        </GridContainer>
+        <div>
+          <GridContainer>
+            <GridItem xs={12} sm={12} md={4}>
+              <InfoArea
+                title="Verified Users"
+                description="Sign up to create an account and start adding your friends."
+                icon={VerifiedUser}
+                iconColor="success"
+                vertical
+              />
+            </GridItem>
+            <GridItem xs={12} sm={12} md={4}>
+              <InfoArea
+                title="Dolphin Extension"
+                description="Download Dolphin Connect's companion Chrome Extension to correctly synchronize shows."
+                icon={Waves}
+                iconColor="info"
+                vertical
+              />
+            </GridItem>
+            <GridItem xs={12} sm={12} md={4}>
+              <InfoArea
+                title="Chat"
+                description="Just log into Netflix and invite your friends to watch something together!"
+                icon={Chat}
+                iconColor="danger"
+                vertical
+              />
+            </GridItem>
+          </GridContainer>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default withStyles(productStyle)(ProductSection);
